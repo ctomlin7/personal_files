728b6669c60616241716c23bdd764cc93692a029
Casey Tomlin <ctomlin5@berkeley.edu>
2025-02-12 23:29:56 -0800

    completed assignments

--- chem_279/hw_1/main.cpp
+++ chem_279/hw_1/main.cpp
@@ -1,19 +1,18 @@
 #include "hw_1.h"
 
 int main(void) {
-    string filename = "/home/ctomlin/chem_279/hw_1/chem_x79_hw1/sample_input/Force/1.txt";
+    string filename = "/home/ctomlin/chem_279/hw_1/chem_x79_hw1/sample_input/SD_with_line_search/2.txt";
     Atoms result(filename);
-    vector<Vector3d> vec = result.read_xyz(filename);
+    vector<vector<double>> vec = result.read_xyz(filename);
 
     LennardJones lj(filename);
-
-    //lj.run_LJ(filename);
-
-    //cout << vec << endl;
+    lj.run_LJ(filename);
+    cout << vec << endl;
 
     FiniteDifference fd(filename);
-
     fd.run_FD(filename);
+
+    fd.steepest_descent(vec);
 
     return 0;
 }
