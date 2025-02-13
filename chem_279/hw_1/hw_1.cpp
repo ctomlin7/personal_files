250049e39f435e154de919f77a190c4e4c6e4b87
Casey Tomlin <ctomlin5@berkeley.edu>
2025-02-12 22:09:07 -0800

    code done, still needs working confirmation

--- chem_279/hw_1/hw_1.cpp
+++ chem_279/hw_1/hw_1.cpp
@@ -12,7 +12,7 @@ FiniteDifference::FiniteDifference(const string &filename)
         :LennardJones(filename)
         { };
 
-vector<Vector3d> Atoms::read_xyz(const string &filename) {
+vector<vector<double>> Atoms::read_xyz(const string &filename) {
     string firstline;
     string line;
     int num_atoms;
@@ -26,8 +26,8 @@ vector<Vector3d> Atoms::read_xyz(const string &filename) {
             istringstream iss(firstline);
             iss >> num_atoms;
         }
-        vector<Vector3d> result;
-        for (int i=0; i<num_atoms; i++) {
+        vector<vector<double>> result;
+        for (int i = 0; i < num_atoms; i++) {
             if (!getline(file, line)) { throw runtime_error("Unexpected end of file."); }
 
             istringstream iss(line);
@@ -38,7 +38,7 @@ vector<Vector3d> Atoms::read_xyz(const string &filename) {
 
             if (!(atom == 79)) { throw runtime_error("This is not a gold atom"); }
 
-            Vector3d v(x, y, z);
+            vector<double> v = {x, y, z};
             result.push_back(v);
         }
         file.close();
@@ -58,9 +58,8 @@ double LennardJones::calculate_LJ(double r_ij) {
 double LennardJones::calculate_distance(AtomCoord coord1, AtomCoord coord2) {
     double distance = 0;
 
-    for(int i=0; i<3; i++) {
+    for (int i = 0; i < 3; i++) {
         double dim_dist = (coord1[i] - coord2[i]);
-
         dim_dist = dim_dist * dim_dist;
         distance += dim_dist;
     }
@@ -71,8 +70,8 @@ double LennardJones::calculate_distance(AtomCoord coord1, AtomCoord coord2) {
 double LennardJones::calculate_total_energy(Coordinates coordinates) {
     double total_energy = 0.0;
 
-    for(int i=0; i<coordinates.size(); i++) {
-        for(int j=i+1; j<coordinates.size(); j++) {
+    for (int i = 0; i < coordinates.size(); i++) {
+        for (int j = i + 1; j < coordinates.size(); j++) {
             double dist_ij = calculate_distance(coordinates[i], coordinates[j]);
             total_energy += calculate_LJ(dist_ij);
         }
@@ -84,7 +83,7 @@ double LennardJones::calculate_pair_energy(Coordinates coordinates, int i_partic
     double e_total = 0.0;
     int num_atoms = coordinates.size();
 
-    for(int j_particle=0; j_particle<num_atoms; j_particle++) {
+    for (int j_particle = 0; j_particle < num_atoms; j_particle++) {
         if (j_particle != i_particle) {
             double dist_ij = calculate_distance(coordinates[i_particle], coordinates[j_particle]);
             double interaction_energy = calculate_LJ(dist_ij);
@@ -96,7 +95,7 @@ double LennardJones::calculate_pair_energy(Coordinates coordinates, int i_partic
 
 void LennardJones::run_LJ(string &filename) {
     Atoms result(filename);
-    vector<Vector3d> vec = result.read_xyz(filename);
+    vector<vector<double>> vec = result.read_xyz(filename);
     LennardJones lj(filename);
     
     double total_energy = lj.calculate_total_energy(vec);
@@ -106,29 +105,28 @@ void LennardJones::run_LJ(string &filename) {
 
 vector<vector<double>> FiniteDifference::analytical_force(Coordinates coordinates) {
     int num_atoms = coordinates.size();
-    vector<vector<double>> force_vec(num_atoms, vector<double>(3, 0.0)); // 3D force vectors
+    Coordinates force_vec(num_atoms, vector<double>(3, 0.0));
 
     for (int i = 0; i < num_atoms; ++i) {
         for (int j = i + 1; j < num_atoms; ++j) {
             vector<double> r_vec(3, 0.0);
             double r_ij = 0.0;
-            
-            for(int i=0; i<coordinates.size(); i++) {
-                for(int j=i+1; j<coordinates.size(); j++) {
-                    r_vec[i] = coordinates[i][i] - coordinates[j][i];
-                    r_ij += calculate_distance(coordinates[i], coordinates[j]);
-                }
-            }
 
-            double r7_term = (pow(sigma, 6.0)/pow(r_ij, 7.0)); 
-            double r13_term = (pow(sigma, 12.0)/pow(r_ij, 13.0));
+            for (int k = 0; k < 3; ++k) {
+                double dim_dist = (coordinates[i][k] - coordinates[j][k]);
+                r_vec[k] = dim_dist;
+                r_ij += dim_dist * dim_dist;
+            }
+            r_ij = sqrt(r_ij);
+
+            double r7_term = (pow(sigma, 6.0) / pow(r_ij, 7.0)); 
+            double r13_term = (pow(sigma, 12.0) / pow(r_ij, 13.0));
             double force_mag = epsilon * ((12 * r13_term) - (12 * r7_term));
 
-            // Apply force to both atoms
             for (int k = 0; k < 3; ++k) {
                 double force_component = -1 * force_mag * r_vec[k] / r_ij;
                 force_vec[i][k] += force_component;
-                //force_vec[j][k] -= force_component;  
+                force_vec[j][k] -= force_component;  
             }
         }
     }
@@ -169,7 +167,7 @@ double FiniteDifference::central_difference(Coordinates coordinates, int i_parti
 
 void FiniteDifference::run_FD(string &filename) {
     Atoms result(filename);
-    vector<Vector3d> vec = result.read_xyz(filename);
+    Coordinates vec = result.read_xyz(filename);
     LennardJones lj(filename);
     FiniteDifference fd(filename);
 
@@ -177,13 +175,13 @@ void FiniteDifference::run_FD(string &filename) {
     
     printFormattedData("F_LJ_Analytical", analytical_force(vec), 0.0);
 
-    for (int i=0; i<h.size(); i++) {
-        vector<vector<double>> F_LJ_forward_difference;
-        vector<vector<double>> F_LJ_central_difference;
-        for(int k=0; k<vec.size(); k++) {
+    for (int i = 0; i < h.size(); i++) {
+        Coordinates F_LJ_forward_difference;
+        Coordinates F_LJ_central_difference;
+        for (int k = 0; k < vec.size(); k++) {
             vector<double> for_row;
             vector<double> cen_row;
-            for(int j=0; j<3; j++) {
+            for (int j = 0; j < 3; j++) {
                 double forward_derivative = fd.forward_difference(vec, k, j, h[i]);
                 double central_derivative = fd.central_difference(vec, k, j, h[i]);
 
@@ -198,7 +196,7 @@ void FiniteDifference::run_FD(string &filename) {
     }
 }
 
-void FiniteDifference::printFormattedData(const string & label, const vector<vector<double>>& data, double stepsize) {
+void FiniteDifference::printFormattedData(const string & label, Coordinates data, double stepsize) {
     cout << label << "   Stepsize: " << setprecision(4) << defaultfloat << stepsize << endl;
     
     for (const auto & row : data) {
@@ -215,9 +213,59 @@ void FiniteDifference::printFormattedData(const string & label, const vector<vec
     cout.flush();
 }
 
-ostream & operator<<(ostream & os, const vector<Vector3d> & vec) {
-    for(const auto& v : vec) {
-        os << "79 (" << v.transpose() << ")" << std::endl;
+void FiniteDifference::steepest_descent(Coordinates coordinates) {
+    Golden golden_search;
+    double displacement;
+    const int max_iteration = 100;
+    int iteration = 0;
+
+    cout << "Initial Energy: " << calculate_total_energy(coordinates) << endl;
+
+    Coordinates new_coords = coordinates;
+    for (int i = 0; i < coordinates.size(); ++i) {
+        for (int j = 0; j < 3; ++j) {
+            new_coords[i][j] -= displacement * central_difference(coordinates, i, j, 1e-4);
+        }
+    }
+    printFormattedData("Central Difference Force", new_coords, 0.0);
+
+    while (iteration < max_iteration) {
+        Coordinates gradient = analytical_force(coordinates);
+        auto line_search_function = [this, &coordinates, &gradient](double displacement) {
+            Coordinates new_coords = coordinates;
+            for (int i = 0; i < coordinates.size(); ++i) {
+                for (int j = 0; j < 3; ++j) {
+                    new_coords[i][j] -= displacement * gradient[i][j];
+                }
+            }
+            return calculate_total_energy(new_coords);
+        };
+
+        golden_search.bracket(0.0, 1.0, line_search_function);
+        displacement = golden_search.minimize(line_search_function);
+
+        for (int i = 0; i < coordinates.size(); ++i) {
+            for (int j = 0; j < 3; ++j) {
+                coordinates[i][j] -= displacement * gradient[i][j];
+            }
+        }
+
+        double energy = calculate_total_energy(coordinates);
+        cout << "Iteration " << iteration << ": Energy = " << energy << endl;
+
+        if (displacement < 1e-6) {
+            break;
+        }
+
+        ++iteration;
+    }
+
+    cout << "Final Energy: " << calculate_total_energy(coordinates) << endl;
+}
+
+ostream & operator<<(ostream & os, const vector<vector<double>> & vec) {
+    for (const auto& v : vec) {
+        os << "79 (" << v[0] << ", " << v[1] << ", " << v[2] << ")" << std::endl;
     }
     return os;
 }
